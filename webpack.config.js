5cdd7b877367ca87db350edbc62cfee980500a34
Germain Boue <boue.germain@gmail.com>
2016-01-09 17:28:52 -0800

    Refreshes browser automatically now when changes are brought to components.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
 
 //map our app to build/bundle.js and generate build/index.html
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS  = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   //Entry accepts a path or an object of entries.
   entry: PATHS.app,
   output: {
@@ -20,3 +23,28 @@ module.exports = {
     })
   ]
 };
+
+if(TARGET === 'start' || !TARGET){
+  module.exports = merge(common, {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      //display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      //parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build'){
+  module.exports = merge(common, {});
+}
